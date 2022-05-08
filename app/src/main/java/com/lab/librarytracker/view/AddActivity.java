599b7ed2dbbedda022d7338c643c84efc1e1101a
Starakira <bangunagung1@gmail.com>
2022-05-08 13:58:11 +0700

    Update

--- app/src/main/java/com/lab/librarytracker/view/AddActivity.java
+++ app/src/main/java/com/lab/librarytracker/view/AddActivity.java
@@ -1,14 +1,16 @@
-package com.lab.librarytracker;
+package com.lab.librarytracker.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.lab.librarytracker.R;
+
+public class AddActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_add);
     }
 }
---
+++ app/src/main/java/com/lab/librarytracker/view/MainActivity.java
@@ -0,0 +1,34 @@
+package com.lab.librarytracker.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.lab.librarytracker.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    FloatingActionButton add_button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyyclerView);
+        add_button = findViewById(R.id.add_button);
+
+        add_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AddActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
